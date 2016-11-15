d1f77a014c71bfef79185041133a810d7e670324
Robert Nica <robynic@gmail.com>
2016-11-15 12:35:10 +0100

    Adjust example controller to the latest API.

--- com.example.app/src/com/example/app/dataviewer/controllers/ExampleControlerCreator.java
+++ com.example.app/src/com/example/app/dataviewer/controllers/ExampleControlerCreator.java
@@ -1,7 +1,10 @@
 package com.example.app.dataviewer.controllers;
 
+import java.util.EnumSet;
+
 import com.biobam.blast2go.api.dataviewer.interfaces.IB2GObjectController;
 import com.biobam.blast2go.api.dataviewer.interfaces.IB2GObjectControllerCreator;
+import com.biobam.blast2go.api.dataviewer.interfaces.WorkbenchProperty;
 import com.example.app.data.ExampleObject;
 
 public class ExampleControlerCreator implements IB2GObjectControllerCreator<ExampleObject> {
@@ -19,6 +22,22 @@ public class ExampleControlerCreator implements IB2GObjectControllerCreator<Exam
 	@Override
 	public Class<? extends IB2GObjectController<?>> getObjectControllerClass() {
 		return ExampleGenericTableDataviewerControler.class;
+	}
+
+	@Override
+	public int getPreferredPositionInMenu() {
+		return 0;
+	}
+
+	@Override
+	public String getViewerName() {
+		return "My Viewer";
+	}
+
+	@Override
+	public EnumSet<WorkbenchProperty> getWorkbenchProperties() {
+		return EnumSet.of(WorkbenchProperty.DEFAULT_VIEWER, WorkbenchProperty.IMPORTANCE_PRIMARY_VIEWER,
+				WorkbenchProperty.POSITION_RESULT);
 	}
 
 }
